f924ff860a898d113d91b5a6d6e1a939d2b32985
Sultan <810601+thysultan@users.noreply.github.com>
2023-07-19 21:16:49 +0300

    works!!!

--- felix.js
+++ felix.js
@@ -1,83 +1,110 @@
+
+const canvas = Object.assign(document.body, {style: 'padding:0;margin:0;'}).appendChild(document.createElement('canvas'));
+const context = canvas.getContext('2d');
 const layout = function (val) {
-  measure(val, 0, '', '', [0, 0, 0, 0], [300, 300, 0, 0])(() => {});
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  measure(val, 0, '', '', [0, 0, 0, 0, 0], [300, 300, 0, 0, 0])(([style, value], [x, y], [w, h]) => {
+    if (value) { context.font = `${style.fontSize} ${style.fontFamily}`; context.fillStyle = style.color; context.fillText(children, x, y + h); }
+    else { context.fillStyle = style.backgroundColor; context.fillRect(x, y, w, h); }
+  });
 }
-const measure = function (val, iii, aaa, jjj, vxy, vwh) {
+const measure = function (val, ddd, aaa, jjj, vxy, vwh) {
   let { type, style, children } = val;
-  let { display = flex } = style;
+  let { display = 'flex' } = style;
   if (display === 'none') return () => {};
-  let { top, right, bottom, left, width, height, gap = 0, flex = 0, margin = [0, 0, 0, 0], padding = [0, 0, 0, 0],
+  let { top, right, bottom, left, width = null, height = null, gap = 0, flex = 0, margin = [0, 0, 0, 0], padding = [0, 0, 0, 0],
     position = 'r', direction = 'c', justifyContent = 's', alignItems = 's', alignSelf = '', aspectRatio = 1 } = style;
   let align = alignItems[2]; // align-items
   let justify = justifyContent[2]; // justify-content
-  let nil = 0;
+  let txt = type === 'text';
   let pos = position[0] === 'r'; // position: relative*, absolute, fixed
-  let dir = +(!(direction[0] === 'r')); // is row/column
-  let rid = +(!dir); // opposite of is row/column
+  let dir = +(!(direction[0] === 'r'));
+  let xxx = +(ddd); // is row/column
+  let yyy = +(!xxx); // opposite of is row/column
   let fff = flex < 0 ? 0 : flex; // flex
-  let mmm = [[margin[3], margin[1]], [margin[0], margin[2]]; // margin
-  let ppp = [[padding[3], padding[1]], padding[0], padding[2]]; // padding
-  let ccc = (justify === 'd' ? children.reverse() : children).flat(); // children
+  let mmm = [ [margin[3], margin[1]], [margin[0], margin[2]] ]; // margin
+  let ppp = [ [padding[3], padding[1]], [padding[0], padding[2]] ]; // padding
+  let ccc = txt ? [] : (justify === 'd' ? children.flat().reverse() : children); // children
   let cxy = [0, 0, 0, 0, 0]; // x/y/length/space/offset
-  let cwh = [width, height, 0, 0, 0]; // w/h/flex/size
-  let imp = cwh[dir] === nil; // implicit size
-  // visit all child nodes
-  for (let idx = 0; idx < ccc.length; ++i) ccc[idx] = measure(ccc[idx], dir, align, justify, cxy, cwh);
+  let cwh = [width, height, 0, 0, 0]; // w/h/flex/used/implict
+  let imp = cwh[xxx] == null; // uses implicit size if relative & has no explicit size
+  for (let idx = 0; idx < ccc.length; ++idx) ccc[idx] = measure(ccc[idx], dir, align, justify, cxy, cwh); // visit all child nodes
   if (pos) { vxy[0x2] += 1; vwh[0x2] += fff; } // accumulate length/flex
-  if (imp) { cwh[dir] = cwh[0x3]; } // use implicit size if not explicity sized
-  cwh[0x4] = pos * imp; // relative & implicit size
-  vwh[0x3] += cwh[dir] + (mmm[dir][0] + mmm[dir][1]); // parent implicit size += (explicit size/implicit size) + margin
-  vwh[rid] = max((cwh[rid] ?? 0) + (mmm[rid][0] + mmm[rid][1]), vwh[rid]); // parent block size = Math.max((parent block size), (explicit size/implicit size) + margin)
+
+  // resolved sizes
+  cwh[xxx] ??= cwh[0x3]; // use implicit inline size if able
+  cwh[yyy] ??= cxy[0x3]; // use implicit block size if able
+
+  // implicit sizes
+  vwh[0x3] = Math.max(vwh[0x3], 0b0) + cwh[xxx] + (mmm[xxx][0] + mmm[xxx][1]); // parent implicit size += (explicit size/implicit size) + margin
+  vxy[0x3] = Math.max(vxy[0x3], 0b00 + cwh[yyy] + (mmm[yyy][0] + mmm[yyy][1])); // parent block size = Math.max((parent block size), (explicit size/implicit size) + margin)
+
   // draw routine
-  return (draw = to => canvas) => {
+  return (draw = () => {}) => {
     init: {
-      cxy[dir] += mmm[dir][0] + ppp[dir][0] + vxy[0x4]; // margin-start + padding.start + cursor
-      cxy[rid] += mmm[rid][0] + ppp[rid][0]; // margin-start + padding.start
+      cxy[xxx] += mmm[xxx][0] + ppp[xxx][0] + vxy[0x4]; // margin-start + padding.start + cursor
+      cxy[yyy] += mmm[yyy][0] + ppp[yyy][0]; // margin-start + padding.start
     }
     size: {
-      cwh[0x3] += ppp[dir][0] + ppp[dir][1] + ((cxy[0x2] - 1) * gap);
-      if (cwh[0x4]) cwh[dir] = (vwh[dir] - vwh[0x3]) * ((fff || 1) / vwh[0x2]); // if implicit sized then size = (total space - used space) * (flex or 1)/(total flex)
+      cwh[0x3] += ppp[xxx][0] + ppp[xxx][1] + ((cxy[0x2] - 1) * gap); // used space += padding + (n * gap)
+      if (pos && imp) cwh[xxx] = (vwh[xxx] - vwh[0x3]) * ((fff || 1) / vwh[0x2]); // if implicit sized then size = (total space - used space) * (flex or 1)/(total flex)
     }
     axis: {
       // poisition: top, right, bottom, left
       for (let [lt, rb, z, xy, wh] of [[left, right, cwh[0x0], 0x0, 0x1], [top, bottom, cwh[0x1], 0x0, 0x1]])
-        if (lt !== nil && rb !== nil && z === nil) { // left, right set while width is undefined
+        if (lt != null && rb != null && z == null) { // left, right set while width is undefined
           cxy[xy] = (pos ? cxy : vxy)[xy] + lt; // node[x/y] = root[x/y] + l/t;
           cwh[wh] = vwh[wh] - lt - rb; // node[w/h] = root[w/h] - l/t - r/b;
-        } else if (lt !== nil) cxy[xy] = (pos ? cxy : vxy)[xy] + lt; // when only l/t, node[x/y] = root/node[x/y] + l/t; dependant on absolute position
-          else if (rb !== nil) cxy[xy] = vxy[xy] - rb + (rel ? 0 : vwh[wh] - cwh[wh]); // when only right/bottom; node[x/y] = root[x/y] - r/b + (root[w\h] - node[w\h]) ...
+        } else if (lt != null) cxy[xy] = (pos ? cxy : vxy)[xy] + lt; // when only l/t, node[x/y] = root/node[x/y] + l/t; dependant on absolute position
+          else if (rb != null) cxy[xy] = vxy[xy] - rb + (rel ? 0 : vwh[wh] - cwh[wh]); // when only right/bottom; node[x/y] = root[x/y] - r/b + (root[w\h] - node[w\h]) ...
           else if (!pos) cxy[xy] = vxy[xy]; // node[x/y] = root[x/y];
       // algin-self / align-items: start*, end, center, stretch
       if (alignSelf) aaa = alignSelf[2]; // by default we assume the aligment of the parents align-items but if there's an align-self we use that
-      let [yx, hw] = [[0x1, 0x1], [0x0, 0x0]][iii]; // root orientation
+      let [yx, hw] = [[0x1, 0x1], [0x0, 0x0]][ddd]; // root orientation
       if (pos) switch (aaa) { // not absolute?
           case 'd': cxy[yx] = vwh[hw] - cwh[hw]; break; // end
           case 'n': cxy[yx] = vwh[hw] / 2 - cwh[hw] / 2; break; // center
           case 'r': cxy[yx] = vwh[hw]; break; // stretch
         }
       // justify: start*, end, center, stretch
-      let [xy, wh] = [[0x0, 0x0], [0x1, 0x1]][iii]; // root orientation
+      let [xy, wh] = [[0x0, 0x0], [0x1, 0x1]][ddd]; // root orientation
       if (pos) switch (jjj) { // not absolute?
           case 'd': cxy[xy] = vwh[wh] - cwh[wh]; break; // end
           case 'n': cxy[xy] = vwh[wh] / 2 - cwh[wh] / 2; break; // center given that mathematically (1 + 1 + 1)/2 is equivalent to (1/2) + (1/2) + (1/2)
           case 'r': cxy[xy] = vwh[wh]; break; // stretch
         }
     }
+
     draw: {
-      draw(val, cxy.map((v, i) => v + vxy[i]), cwh); // draw here
+      draw([style, (txt && children)], cxy.map((v, i) => v + vxy[i]), cwh.map(v => v));
     }
     walk: {
-      for (let fun of ccc) fun();
+      for (let fun of ccc) fun(draw);
     }
     move: {
-      cxy[dir] += mmm[rid][1] + ppp[rid][1] + cwh[dir]; // margin.end + padding.end + width
-      vxy[0x4] += cxy[dir]; // // move cursor
+      cxy[xxx] += mmm[yyy][1] + ppp[yyy][1] + cwh[xxx]; // margin.end + padding.end + width
+      vxy[0x4] += cxy[xxx]; // move cursor
     }
   }
 }
 
+
 // playground =========================
 
-/* Swift style vnode creation */
+
+const s = () => {
+  canvas.width = (innerWidth / 1) * (devicePixelRatio / devicePixelRatio);
+  canvas.height = (innerHeight / 1) * (devicePixelRatio / devicePixelRatio);
+};
+const h = (style, ...children) => ({type: 'view', style, children: children.flat()})
+const r = (_ = s()) => layout(
+  h({ width: innerWidth, height: innerHeight, direction: 'row', backgroundColor: 'red' },
+    h({ flex: 1, backgroundColor: 'blue', height: innerHeight/4, }),
+    h({ flex: 2, backgroundColor: 'green', height: innerHeight/2, }),
+  ),
+)
+setInterval(r, 100);
+/* Swift style node creation */
 function Bar () {
   // string type
   return ('text')
@@ -97,7 +124,6 @@ function Bar () {
       ('text').style('font-size: 12px;').child('World'),
     )
 }
-
 /* Made possible by */
 // for (let constructor of [String, Function]) {
 //   const PROPS = Symbol.for('@@props');
